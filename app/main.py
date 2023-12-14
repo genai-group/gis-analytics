5ea13ecc2d75a8d6ac75cf83d707ef2248fd0a8a
Matt Poulton <getgenai@gmail.com>
2023-12-14 16:38:01 -0700

    getting the dockerfile running

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+# Makes the server accessible from the host machine
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 #%%
 # from config.init import *
 
